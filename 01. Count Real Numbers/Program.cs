0ce75451602304bb9a75d8836d97f6e7fb256011
Kristiyan Stoyanov <93555141+Storm603@users.noreply.github.com>
2022-03-20 16:05:38 +0200

    Add files via upload

---
+++ 01. Count Real Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> ocurances = new SortedDictionary<int, int>();
+
+            foreach (int number in array)
+            {
+                if (!ocurances.ContainsKey(number))
+                {
+                    ocurances.Add(number, 1);
+                }
+                else
+                {
+                    ocurances[number]++;
+                }
+                
+
+            }
+
+            foreach (var ocurance in ocurances)
+            {
+                Console.WriteLine($"{ocurance.Key} -> {ocurance.Value}");
+
+            }
+
+        }
+    }
+}
---
+++ 02. Odd Occurances/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = Console.ReadLine().ToLower().Split().ToArray();
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach (string word in array)
+            {
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, 1);
+                }
+                else
+                {
+                    words[word]++;
+                }
+            }
+
+            foreach (var word in words)
+            {
+                if (word.Value % 2 == 1)
+                {
+                    Console.Write(word.Key + " ");
+                }
+            }
+
+        }
+    }
+}
